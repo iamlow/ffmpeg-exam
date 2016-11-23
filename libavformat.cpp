22e1d9c4bfed0912bb0b535af9603fe6cfb2eae1
iamlow <iamlow@gmail.com>
2016-11-23 16:42:18 +0900

    - libavformat 라이브러리를 가지고 네트워크 카메라에서 영상 가져오기 - RTSP URL을 가지고 테스트

--- libavformat.cpp
+++ libavformat.cpp
@@ -1,12 +1,57 @@
-#include <stdio.h>
+#include <iostream>
 
 extern "C" {
-#include <libavdevice/avdevice.h>
 #include <libavformat/avformat.h>
+#include <libavutil/error.h>
 }
 
-int main(void)
+int main(int argc, char *argv[])
 {
+    if (2 != argc) {
+        std::cout << "invalid argument!\n";
+        return 0;
+    }
+
+    const std::string uri = argv[1];
+    std::cout << "uri: " << uri;
+
     av_register_all();
+
+    int errnum;
+    char errstr[AV_ERROR_MAX_STRING_SIZE];
+    int rc = 0;
+
+    AVFormatContext *fmtCtx = NULL;
+    rc = avformat_open_input(&fmtCtx, uri.c_str(), NULL, NULL);
+    if (0 > rc) {
+        std::cout << "avformat_open_input:"
+            << av_make_error_string(errstr, AV_ERROR_MAX_STRING_SIZE, errnum)
+            << ":" << rc;
+        return 0;
+    }
+
+    rc = avformat_find_stream_info(fmtCtx, NULL);
+    if (0 > rc) {
+        std::cout << "avformat_find_stream_info:"
+            << av_make_error_string(errstr, AV_ERROR_MAX_STRING_SIZE, errnum)
+            << ":" << rc;
+        return rc;
+    }
+
+    for (int i = 0; i < 100; ++i) {
+        AVPacket packet;
+        int rc = av_read_frame(fmtCtx, &packet);
+        if (0 > rc) {
+            std::cout << "av_read_frame:"
+                << av_make_error_string(errstr, AV_ERROR_MAX_STRING_SIZE, errnum)
+                << ":" << rc;
+            break;
+        }
+        std::cout << "read_frame size: " << packet.size << std::endl;
+    }
+
+    avformat_close_input(&fmtCtx);
+    avformat_network_deinit();
+
     return 0;
 }
